e66e47dc33fa31df5e75ad852b39183e763d5d83
combo <alexvarboffin@gmail.com>
2025-02-02 13:47:07 +0300

    Добавил все изменения

---
+++ app/keystore/pem.cmd
@@ -0,0 +1,2 @@
+keytool.exe -export -rfc -v -keystore brand57.jks -alias brand57 -file upload_certificate.pem -storepass brand57
+pause
---
+++ app/keystore/pem_knopka.cmd
@@ -0,0 +1,2 @@
+keytool.exe -export -rfc -v -keystore brand57.jks -alias knopka -file upload_certificate.pem -storepass brand57
+pause
